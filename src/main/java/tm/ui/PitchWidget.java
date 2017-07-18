3bbc8f770b9620f46bab77273fd5bfe28e7999f4
MusinM <mmusin@seznam.cz>
2017-07-18 16:55:02 +0300

    Refactored PitchWidget and UiMatchManager (encapsulation and naming convention).

--- src/main/java/tm/ui/PitchWidget.java
+++ src/main/java/tm/ui/PitchWidget.java
@@ -13,30 +13,31 @@ import org.eclipse.swt.internal.win32.DRAWITEMSTRUCT;
 
 import tm.lib.engine.*;
 
-public class PitchWidget extends Canvas
-{
+public class PitchWidget extends Canvas {
+    
+    private static final int HMARGIN = 5;
+    private static final int VMARGIN = 10;
+    
     private static final double PLAYER_SIZE = 15;
     private static final double BALL_SIZE = 6;
     private static final double TARGET_SIZE = 2;
     private static final double VISIBLE_TARGET_SIZE = 8;
-    
-    private static final boolean DRAW_BLOCKED_ZONES = true;
-    
-    public Label top_label;
-    public Label bottom_label;
-    Pitch pitch;
-    Point pitch_size;
-    Point pitch_pos;
-    public static final int HMARGIN = 5;
-    public static final int VMARGIN = 10;
-    final Color PITCH_COLOR = new Color(TenisMasters.display, 254, 251, 126);
-    final Color DARK_PITCH_COLOR = new Color(TenisMasters.display, 203, 200, 75);
-    final Color LABEL_COLOR = TenisMasters.display.getSystemColor(SWT.COLOR_WHITE);
-    Font font;
-	//Image buffer;
 
-    public PitchWidget(Composite parent, Pitch p)
-    {
+    private static final boolean DRAW_BLOCKED_ZONES = true;
+
+    private Label topLabel;
+    private Label bottomLabel;
+    private Pitch pitch;
+    private Point pitchSize;
+    private Point pitchPos;
+
+    private final Color PITCH_COLOR = new Color(TenisMasters.display, 254, 251, 126);
+    private final Color DARK_PITCH_COLOR = new Color(TenisMasters.display, 203, 200, 75);
+    private final Color LABEL_COLOR = TenisMasters.display.getSystemColor(SWT.COLOR_WHITE);
+    private Font font;
+    //Image buffer;
+
+    public PitchWidget(Composite parent, Pitch p) {
         super(parent, SWT.BORDER | SWT.DOUBLE_BUFFERED);
         pitch = p;
 
@@ -44,10 +45,10 @@ public class PitchWidget extends Canvas
         layout.verticalSpacing = 10;
         setLayout(layout);
 
-        top_label = new Label(this, SWT.CENTER | SWT.BORDER);
-        top_label.setText("---");
+        topLabel = new Label(this, SWT.CENTER | SWT.BORDER);
+        topLabel.setText("---");
         //top_label.setBackground(PITCH_COLOR);
-        top_label.setBackground(LABEL_COLOR);
+        topLabel.setBackground(LABEL_COLOR);
 
         GridData data = new GridData();
         //data.horizontalAlignment = GridData.FILL;
@@ -55,84 +56,77 @@ public class PitchWidget extends Canvas
         data.verticalAlignment = GridData.END;
         data.grabExcessHorizontalSpace = true;
         data.grabExcessVerticalSpace = true;
-        top_label.setLayoutData(data);
+        topLabel.setLayoutData(data);
 
-        bottom_label = new Label(this, SWT.CENTER | SWT.BORDER);
-        bottom_label.setText("+++");
-        bottom_label.setBackground(LABEL_COLOR);
+        bottomLabel = new Label(this, SWT.CENTER | SWT.BORDER);
+        bottomLabel.setText("+++");
+        bottomLabel.setBackground(LABEL_COLOR);
         data = new GridData();
         data.horizontalAlignment = GridData.CENTER;
         data.verticalAlignment = GridData.BEGINNING;
         data.grabExcessVerticalSpace = true;
         data.grabExcessHorizontalSpace = true;
-        bottom_label.setLayoutData(data);
+        bottomLabel.setLayoutData(data);
 
-        FontData[] font_data = top_label.getFont().getFontData();
+        FontData[] font_data = topLabel.getFont().getFontData();
         font_data[0].setHeight(18);
         font = new Font(TenisMasters.display, font_data[0]);
-        top_label.setFont(font);
-        bottom_label.setFont(font);
+        topLabel.setFont(font);
+        bottomLabel.setFont(font);
 
-        top_label.setVisible(false);
-        bottom_label.setVisible(false);
+        topLabel.setVisible(false);
+        bottomLabel.setVisible(false);
 
         pack();
 
-		//buffer = new Image(TenisMasters.display, getClientArea());
-        addDisposeListener(new DisposeListener()
-        {
-            public void widgetDisposed(DisposeEvent e)
-            {
+        //buffer = new Image(TenisMasters.display, getClientArea());
+        addDisposeListener(new DisposeListener() {
+            public void widgetDisposed(DisposeEvent e) {
                 font.dispose();
                 //buffer.dispose();
             }
         });
 
-        addPaintListener(new PaintListener()
-        {
-            public void paintControl(PaintEvent e)
-            {
-				//update_buffer();
+        addPaintListener(new PaintListener() {
+            public void paintControl(PaintEvent e) {
+                //update_buffer();
                 //buffer = new Image(TenisMasters.display, getClientArea().width, getClientArea().height);
                 //GC gc = new GC(buffer);
                 //draw_pitch(gc);
                 //e.gc.drawImage(buffer, 0, 0);
                 //buffer = new Image(TenisMasters.display, getClientArea());
-                draw_pitch(e.gc);
+                drawPitch(e.gc);
             }
         });
     }
 
-    public void set_upper_text(String text)
-    {
-        top_label.setText(text);
-        top_label.pack();
+    public void setUpperText(String text) {
+        topLabel.setText(text);
+        topLabel.pack();
         GridData data = new GridData();
         data.horizontalAlignment = GridData.CENTER;
         data.verticalAlignment = GridData.END;
         data.grabExcessHorizontalSpace = true;
         data.grabExcessVerticalSpace = true;
-        top_label.setLayoutData(data);
-        top_label.setVisible(true);
+        topLabel.setLayoutData(data);
+        topLabel.setVisible(true);
         layout();
     }
 
-    public void set_bottom_text(String text)
-    {
-        bottom_label.setText(text);
-        bottom_label.pack();
+    public void setBottomText(String text) {
+        bottomLabel.setText(text);
+        bottomLabel.pack();
         GridData data = new GridData();
         data.horizontalAlignment = GridData.CENTER;
         data.verticalAlignment = GridData.BEGINNING;
         data.grabExcessHorizontalSpace = true;
         data.grabExcessVerticalSpace = true;
-        bottom_label.setLayoutData(data);
-        bottom_label.setVisible(true);
+        bottomLabel.setLayoutData(data);
+        bottomLabel.setVisible(true);
         layout();
     }
 
-    private void update_pitch_pos()
-    {
+    private void updatePitchPos() {
         Point s = getSize();
         s.x -= 2 * HMARGIN;
         s.y -= 2 * VMARGIN;
@@ -141,47 +135,41 @@ public class PitchWidget extends Canvas
         double coef = s.y / Pitch.HEIGHT;
         psize = psize.scalarMultiply(coef);
 
-        if ((int) psize.getX() <= s.x)
-        {
-            pitch_size = VectorUtils.toPoint(psize);
-            pitch_pos = new Point((s.x - pitch_size.x) / 2 + HMARGIN, VMARGIN);
-        }
-        else
-        {
+        if ((int) psize.getX() <= s.x) {
+            pitchSize = VectorUtils.toPoint(psize);
+            pitchPos = new Point((s.x - pitchSize.x) / 2 + HMARGIN, VMARGIN);
+        } else {
             coef = s.x / Pitch.WIDTH;
             psize = new Vector2D(Pitch.WIDTH, Pitch.HEIGHT);
             psize = psize.scalarMultiply(coef);
-            pitch_size = VectorUtils.toPoint(psize);
-            pitch_pos = new Point(HMARGIN, (s.y - pitch_size.y) / 2 + VMARGIN);
+            pitchSize = VectorUtils.toPoint(psize);
+            pitchPos = new Point(HMARGIN, (s.y - pitchSize.y) / 2 + VMARGIN);
         }
     }
 
-    private Point widget_pos(double x, double y)
-    {
-        double coef = pitch_size_coef();
-        int px = (int) (x * coef + pitch_pos.x);
-        int py = (int) ((Pitch.HALF_HEIGHT - y) * coef) + pitch_pos.y;
+    private Point mapPos(double x, double y) {
+        double coef = getPitchSizeCoef();
+        int px = (int) (x * coef + pitchPos.x);
+        int py = (int) ((Pitch.HALF_HEIGHT - y) * coef) + pitchPos.y;
         return new Point(px, py);
     }
-    
-    private Point widget_pos(Vector2D pos)
-    {
-        return widget_pos(pos.getX(), pos.getY());
-    }
-    
-    private Rectangle widget_rect(double x1, double y1, double x2, double y2) {
-        Point p1 = widget_pos(x1, y1);
-        Point p2 = widget_pos(x2, y2);
-        return new Rectangle(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
-    }
-    
-    private Rectangle widget_rect(Vector2D topLeft, Vector2D bottomRight) {
-        return widget_rect(topLeft.getX(), topLeft.getY(), bottomRight.getX(), bottomRight.getY());
+
+    private Point mapPos(Vector2D pos) {
+        return mapPos(pos.getX(), pos.getY());
     }
 
-    private double pitch_size_coef()
-    {
-        return pitch_size.x / Pitch.WIDTH;
+    private Rectangle mapRect(double x1, double y1, double x2, double y2) {
+        Point p1 = mapPos(x1, y1);
+        Point p2 = mapPos(x2, y2);
+        return new Rectangle(p1.x, p1.y, p2.x - p1.x, p2.y - p1.y);
+    }
+
+    private Rectangle mapRect(Vector2D topLeft, Vector2D bottomRight) {
+        return mapRect(topLeft.getX(), topLeft.getY(), bottomRight.getX(), bottomRight.getY());
+    }
+
+    private double getPitchSizeCoef() {
+        return pitchSize.x / Pitch.WIDTH;
     }
 
     /*public void update_buffer() {
@@ -194,8 +182,7 @@ public class PitchWidget extends Canvas
      buffer.setBackground(getBackground());
      }
      }*/
-    public void update()
-    {
+    public void update() {
         redraw();
 
         /*final Image buffer = new Image(TenisMasters.display, this.getClientArea());
@@ -206,15 +193,15 @@ public class PitchWidget extends Canvas
          buffer.dispose();
          gc.dispose();
          pitch_gc.dispose();*/
-		//update_buffer();
-		/*draw_pitch_borders(gc);
+        //update_buffer();
+        /*draw_pitch_borders(gc);
          draw_player(gc, pitch.player_1);
          draw_player(gc, pitch.player_2);
          draw_ball(gc);*/
-		//draw_pitch(gc);
+        //draw_pitch(gc);
         //update_pitch_pos();
         //this.redraw(pitch_pos.x, pitch_pos.y, pitch_size.x, pitch_size.y, false);
-		/*gc.setBackground(PITCH_COLOR); 
+        /*gc.setBackground(PITCH_COLOR); 
          gc.fillRectangle(pitch_pos.x + 1, pitch_pos.y + 1, pitch_size.x - 2, pitch_size.y / 2 - 2);
          gc.fillRectangle(pitch_pos.x + 1, pitch_pos.y + pitch_size.y / 2 + 1, pitch_size.x - 2, pitch_size.y / 2 - 3);		
          draw_player(gc, pitch.player_1);
@@ -222,51 +209,44 @@ public class PitchWidget extends Canvas
          draw_ball(gc);		*/
     }
 
-    public void draw_pitch(GC gc)
-    {
-        update_pitch_pos();
-        draw_pitch_borders(gc);
-        draw_player(gc, pitch.getPlayer(Side.HOME));
-        draw_player(gc, pitch.getPlayer(Side.AWAY));
-        draw_ball(gc);
+    public void drawPitch(GC gc) {
+        updatePitchPos();
+        drawPitchBorders(gc);
+        drawPlayer(gc, pitch.getPlayer(Side.HOME));
+        drawPlayer(gc, pitch.getPlayer(Side.AWAY));
+        drawBall(gc);
     }
 
-    public void draw_pitch_borders(GC gc)
-    {
+    public void drawPitchBorders(GC gc) {
         gc.setBackground(PITCH_COLOR);
         gc.setLineWidth(2);
-        Rectangle pitchRectangle = widget_rect(0, Pitch.HALF_HEIGHT, Pitch.WIDTH, -Pitch.HALF_HEIGHT);
+        Rectangle pitchRectangle = mapRect(0, Pitch.HALF_HEIGHT, Pitch.WIDTH, -Pitch.HALF_HEIGHT);
         gc.fillRectangle(pitchRectangle);
         gc.setBackground(DARK_PITCH_COLOR);
 
         if (DRAW_BLOCKED_ZONES) {
             double homePlayerBlockedZoneLength = pitch.calculateNetBlockedZoneLength(pitch.getPlayer(Side.HOME).getPosition());
-            gc.fillRectangle(widget_rect(0, 0, Pitch.WIDTH, -homePlayerBlockedZoneLength));
+            gc.fillRectangle(mapRect(0, 0, Pitch.WIDTH, -homePlayerBlockedZoneLength));
             double awayPlayerBlockedZoneLength = pitch.calculateNetBlockedZoneLength(pitch.getPlayer(Side.AWAY).getPosition());
-            gc.fillRectangle(widget_rect(0, awayPlayerBlockedZoneLength, Pitch.WIDTH, 0));
+            gc.fillRectangle(mapRect(0, awayPlayerBlockedZoneLength, Pitch.WIDTH, 0));
         }
-        
+
         gc.setForeground(TenisMasters.display.getSystemColor(SWT.COLOR_DARK_RED));
         gc.drawRectangle(pitchRectangle);
-        gc.drawLine(pitch_pos.x, pitch_pos.y + pitch_size.y / 2, pitch_pos.x + pitch_size.x, pitch_pos.y + pitch_size.y / 2);
+        gc.drawLine(pitchPos.x, pitchPos.y + pitchSize.y / 2, pitchPos.x + pitchSize.x, pitchPos.y + pitchSize.y / 2);
     }
 
-    public void draw_player(GC gc, Player p)
-    {
-        double coef = pitch_size_coef();
+    public void drawPlayer(GC gc, Player p) {
+        double coef = getPitchSizeCoef();
         int radius = (int) (coef * PLAYER_SIZE / 2);
-        if (radius < 1)
-        {
+        if (radius < 1) {
             radius = 1;
         }
-        Point pos = widget_pos(p.getPosition());
-        if (!p.isLying())
-        {
+        Point pos = mapPos(p.getPosition());
+        if (!p.isLying()) {
             gc.setForeground(TenisMasters.display.getSystemColor(SWT.COLOR_DARK_BLUE));
             gc.setBackground(TenisMasters.display.getSystemColor(SWT.COLOR_BLUE));
-        }
-        else
-        {
+        } else {
             gc.setForeground(TenisMasters.display.getSystemColor(SWT.COLOR_DARK_RED));
             gc.setBackground(TenisMasters.display.getSystemColor(SWT.COLOR_RED));
         }
@@ -275,36 +255,37 @@ public class PitchWidget extends Canvas
         gc.drawOval(pos.x - radius, pos.y - radius, radius * 2, radius * 2);
     }
 
-    public void draw_ball(GC gc)
-    {
+    public void drawBall(GC gc) {
         Ball ball = pitch.getBall();
-        Point pos = widget_pos(ball.getPosition());
-        double coef = pitch_size_coef();
+        Point pos = mapPos(ball.getPosition());
+        double coef = getPitchSizeCoef();
         int radius = (int) (coef * BALL_SIZE / 2);
-        if (radius < 1)
-        {
+        if (radius < 1) {
             radius = 1;
         }
         gc.setBackground(TenisMasters.display.getSystemColor(SWT.COLOR_BLACK));
         gc.setForeground(TenisMasters.display.getSystemColor(SWT.COLOR_BLACK));
         gc.fillOval(pos.x - radius, pos.y - radius, radius * 2, radius * 2);
 
-        pos = widget_pos(ball.getRealTarget());
+        pos = mapPos(ball.getRealTarget());
         int size = (int) (coef * TARGET_SIZE / 2);
-        if (size < 1)
-        {
+        if (size < 1) {
             size = 1;
         }
         gc.drawOval(pos.x - size, pos.y - size, size * 2, size * 2);
 
-        pos = widget_pos(ball.getVisibleTarget());
+        pos = mapPos(ball.getVisibleTarget());
         size = (int) (coef * VISIBLE_TARGET_SIZE / 2);
-        if (size < 1)
-        {
+        if (size < 1) {
             size = 1;
         }
         gc.setLineWidth(1);
         gc.drawLine(pos.x - size, pos.y - size, pos.x + size, pos.y + size);
         gc.drawLine(pos.x - size, pos.y + size, pos.x + size, pos.y - size);
+    }
+    
+    public void hideResultCaption() {
+        topLabel.setVisible(false);
+        bottomLabel.setVisible(false);
     }
 }
--- src/main/java/tm/ui/UiMatchManager.java
+++ src/main/java/tm/ui/UiMatchManager.java
@@ -17,8 +17,8 @@ import tm.lib.engine.Side;
  *
  * @author Corwine
  */
-public class UiMatchManager
-{
+public class UiMatchManager {
+
     public Match match;
     private MatchSimulator matchSimulator;
     private MatchSimulator.State currentState;
@@ -30,23 +30,19 @@ public class UiMatchManager
     public Runnable match_end_timer;
     public MatchWindow ui;
 
-
     //static public final int TIMER_INTERVAL = 30;
     static public final int TIMER_INTERVAL = (int) (1000 * MatchEngine.TIME_STEP);
     static public final int CAPTION_TIME = 1500;
     static public final int CAPTION_TICKS = (int) (CAPTION_TIME / TIMER_INTERVAL);
 
-    public UiMatchManager(MatchWindow match_window, Match match)
-    {
+    public UiMatchManager(MatchWindow match_window, Match match) {
         this.match = match;
         matchSimulator = new MatchSimulator(match);
         ui = match_window;
-        
-        game_timer = new Runnable()
-        {
+
+        game_timer = new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 currentState = matchSimulator.proceed();
                 /*if (currentState == MatchSimulator.State.PLAYING) {
                     currentState = matchSimulator.proceed();
@@ -55,211 +51,157 @@ public class UiMatchManager
                 ui.player1_widget.update();
                 ui.player2_widget.update();
                 ui.match_info_widget.set_time(matchSimulator.getMatchTime());
-                if (currentState != MatchSimulator.State.PLAYING)
-                {
+                if (currentState != MatchSimulator.State.PLAYING) {
                     game_has_ended();
-                }
-                else
-                {
+                } else {
                     TenisMasters.display.timerExec(TIMER_INTERVAL, this);
                 }
             }
         };
 
-        game_end_timer = new Runnable()
-        {
+        game_end_timer = new Runnable() {
             public int i = 0;
 
             @Override
-            public void run()
-            {
+            public void run() {
                 i++;
-                if (i == CAPTION_TICKS)
-                {
+                if (i == CAPTION_TICKS) {
                     hide_result_caption();
                     i = 0;
                     game_end_actions();
-                }
-                else
-                {
+                } else {
                     TenisMasters.display.timerExec(TIMER_INTERVAL, this);
                 }
             }
         };
 
-        set_end_timer = new Runnable()
-        {
+        set_end_timer = new Runnable() {
             public int i = 0;
 
             @Override
-            public void run()
-            {
+            public void run() {
                 i++;
-                if (i == CAPTION_TICKS)
-                {
+                if (i == CAPTION_TICKS) {
                     hide_result_caption();
                     i = 0;
                     set_end_actions();
-                }
-                else
-                {
+                } else {
                     TenisMasters.display.timerExec(TIMER_INTERVAL, this);
                 }
             }
         };
 
-        match_end_timer = new Runnable()
-        {
+        match_end_timer = new Runnable() {
             public int i = 0;
 
             @Override
-            public void run()
-            {
+            public void run() {
                 i++;
-                if (i == CAPTION_TICKS)
-                {
+                if (i == CAPTION_TICKS) {
                     hide_result_caption();
                     i = 0;
                     match_end_actions();
-                }
-                else
-                {
+                } else {
                     TenisMasters.display.timerExec(TIMER_INTERVAL, this);
                 }
             }
         };
     }
 
-    public void start()
-    {
+    public void start() {
         active_timer = game_timer;
         TenisMasters.display.timerExec(TIMER_INTERVAL, active_timer);
     }
 
-    public void pause()
-    {
+    public void pause() {
         TenisMasters.display.timerExec(-1, active_timer);
     }
-    
-    public Pitch getPitch()
-    {
+
+    public Pitch getPitch() {
         return matchSimulator.getPitch();
     }
 
-    private void game_has_ended()
-    {
+    private void game_has_ended() {
         show_game_result_caption();
         active_timer = game_end_timer;
         TenisMasters.display.timerExec(TIMER_INTERVAL, active_timer);
     }
 
-    private void game_end_actions()
-    {
-        if (currentState != MatchSimulator.State.GAME_ENDED)
-        {
+    private void game_end_actions() {
+        if (currentState != MatchSimulator.State.GAME_ENDED) {
             show_set_result_caption();
             active_timer = set_end_timer;
             TenisMasters.display.timerExec(TIMER_INTERVAL, active_timer);
-        }
-        else
-        {
+        } else {
             start();
         }
     }
 
-    private void set_end_actions()
-    {
-        if (currentState != MatchSimulator.State.MATCH_ENDED)
-        {
+    private void set_end_actions() {
+        if (currentState != MatchSimulator.State.MATCH_ENDED) {
             start();
-        }
-        else
-        {
+        } else {
             show_match_result_caption();
             active_timer = match_end_timer;
             TenisMasters.display.timerExec(TIMER_INTERVAL, active_timer);
         }
     }
 
-    private void match_end_actions()
-    {
+    private void match_end_actions() {
         ui.start_button.setEnabled(false);
         ui.final_score = matchSimulator.getCurrentScore();
     }
 
-    private void show_game_result_caption()
-    {
+    private void show_game_result_caption() {
         Person won;
-        if (matchSimulator.getLastGameResult() == Side.HOME)
-        {
+        if (matchSimulator.getLastGameResult() == Side.HOME) {
             won = match.getFirstPlayer();
-        }
-        else
-        {
+        } else {
             won = match.getSecondPlayer();
         }
         ui.match_info_widget.score_label.setText(matchSimulator.getCurrentScore().toString());
         ui.match_info_widget.score_label.pack();
         //ui.match_info_widget.score_label.pack();
         ui.shell.layout();
-        ui.pitch_widget.set_upper_text("Игрок " + won.getFullName() + " выиграл гейм.");
-        ui.pitch_widget.set_bottom_text("Счёт в игре: " + matchSimulator.getCurrentScore());
+        ui.pitch_widget.setUpperText("Игрок " + won.getFullName() + " выиграл гейм.");
+        ui.pitch_widget.setBottomText("Счёт в игре: " + matchSimulator.getCurrentScore());
     }
 
-    private void show_set_result_caption()
-    {
-        Person won = (matchSimulator.getLastGameResult()== Side.HOME) ? match.getFirstPlayer() : match.getSecondPlayer();
-        ui.pitch_widget.set_upper_text("Сет разыгран, победитель -  " + won.getFullName());
-        ui.pitch_widget.set_bottom_text("Счёт в игре: " + matchSimulator.getCurrentScore());
+    private void show_set_result_caption() {
+        Person won = (matchSimulator.getLastGameResult() == Side.HOME) ? match.getFirstPlayer() : match.getSecondPlayer();
+        ui.pitch_widget.setUpperText("Сет разыгран, победитель -  " + won.getFullName());
+        ui.pitch_widget.setBottomText("Счёт в игре: " + matchSimulator.getCurrentScore());
     }
 
-    private void show_match_result_caption()
-    {
+    private void show_match_result_caption() {
         Person won;
         BasicScore s = matchSimulator.getCurrentScore().getScoreBySets();
-        if (s.v1 > s.v2)
-        {
+        if (s.v1 > s.v2) {
             won = match.getFirstPlayer();
-        }
-        else
-        {
-            if (s.v1 < s.v2)
-            {
+        } else {
+            if (s.v1 < s.v2) {
                 won = match.getSecondPlayer();
-            }
-            else
-            {
-                if (match.isPlayoff())
-                {
-                    if (matchSimulator.getCurrentScore().getAdditionalTime().v1 > matchSimulator.getCurrentScore().getAdditionalTime().v2)
-                    {
+            } else {
+                if (match.isPlayoff()) {
+                    if (matchSimulator.getCurrentScore().getAdditionalTime().v1 > matchSimulator.getCurrentScore().getAdditionalTime().v2) {
                         won = match.getFirstPlayer();
-                    }
-                    else
-                    {
+                    } else {
                         won = match.getSecondPlayer();
                     }
-                }
-                else
-                {
+                } else {
                     won = null;
                 }
             }
         }
-        if (won != null)
-        {
-            ui.pitch_widget.set_upper_text("Матч завершён, победитель -  " + won.getFullName());
+        if (won != null) {
+            ui.pitch_widget.setUpperText("Матч завершён, победитель -  " + won.getFullName());
+        } else {
+            ui.pitch_widget.setUpperText("Матч завершён, итоговый результат - ничья");
         }
-        else
-        {
-            ui.pitch_widget.set_upper_text("Матч завершён, итоговый результат - ничья");
-        }
-        ui.pitch_widget.set_bottom_text("Счёт в игре: " + matchSimulator.getCurrentScore());
+        ui.pitch_widget.setBottomText("Счёт в игре: " + matchSimulator.getCurrentScore());
     }
 
-    private void hide_result_caption()
-    {
-        ui.pitch_widget.top_label.setVisible(false);
-        ui.pitch_widget.bottom_label.setVisible(false);
+    private void hide_result_caption() {
+        ui.pitch_widget.hideResultCaption();
     }
 }
